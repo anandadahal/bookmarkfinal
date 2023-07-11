ff6a82acca2bf53bd228bd7897ebe8ee58752b1a
ananda dahal <106259349+anandadahal@users.noreply.github.com>
2023-07-11 12:10:57 +0545

    Update TodoForm.js

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,3 +1,112 @@
+// import React, { useEffect, useState } from "react";
+// import { v4 as uuidv4 } from "uuid";
+// import { account, databases } from "../appwrite/appwriteConfig";
+
+// function TodoForm() {
+//   const [todo, setTodo] = useState("");
+//   const [userId, setUserId] = useState(null);
+
+//   useEffect(() => {
+//     account
+//       .get()
+//       .then((response) => {
+//         setUserId(response.$id);
+//         console.log(response);
+//       })
+//       .catch((errror) => {
+//         console.log("error");
+//       });
+//   }, [userId]);
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     const promise = databases.createDocument(
+//       "64996799eb3a0c0a2089",
+//       "649967a0055f70f24542",
+//       uuidv4(),
+//       {
+//         userId,
+//         todo,
+//       }
+//     );
+//     console.log(promise);
+//     promise.then(
+//       function (response) {
+//         window.location.reload();
+//         console.log(response); // Success
+//       },
+//       function (error) {
+//         console.log(error); // Failure
+//       }
+//     );
+//     e.target.reset();
+//   };
+
+//   const [formData, setFormData] = useState({
+//     userInput: "",
+//   });
+
+//   const [errorMessage, setErrorMessage] = useState("");
+
+//   const { userInput } = formData;
+
+//   const handleChange = (e) => {
+//     const { value } = e.target;
+//     setFormData({ userInput: value });
+//     setErrorMessage("");
+//   };
+
+//   const handleSubmi = (e) => {
+//     e.preventDefault();
+
+//     if (userInput.trim() === "") {
+//       setErrorMessage("Please enter data.");
+//       return;
+//     }
+
+//     // Handle form submission logic here
+//   };
+//   return (
+//     <>
+//       <div className="max-w-7xl mx-auto mt-10">
+//         <form
+//           action=""
+//           onSubmit={handleSubmit}
+//           className="flex justify-center mb-10"
+//         >
+//           <input
+//             id="input"
+//             name="input"
+//             type="url"
+//             placeholder="Eg:https://www.facebook.com"
+//             className="border p-2 w-2/3 rounded-md  text-white"
+//             onChange={(e) => {
+//               setTodo(e.target.value);
+//             }}
+//           />
+//           <button
+//             type="submit"
+//             className="bg-blue-500 p-2 text-white ml-2 rounded-md"
+//           >
+//             ADD BOOKMARK
+//           </button>
+//         </form>
+//       </div>
+//       -
+//     </>
+//   );
+// }
+
+// export default TodoForm;
+
+
+
+
+
+
+
+
+
 import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { account, databases } from "../appwrite/appwriteConfig";
@@ -5,6 +114,7 @@ import { account, databases } from "../appwrite/appwriteConfig";
 function TodoForm() {
   const [todo, setTodo] = useState("");
   const [userId, setUserId] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     account
@@ -13,13 +123,19 @@ function TodoForm() {
         setUserId(response.$id);
         console.log(response);
       })
-      .catch((errror) => {
+      .catch((error) => {
         console.log("error");
       });
   }, [userId]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (todo.trim() === "") {
+      setErrorMessage("Please enter data.");
+      return;
+    }
+
     const promise = databases.createDocument(
       "64996799eb3a0c0a2089",
       "649967a0055f70f24542",
@@ -41,6 +157,7 @@ function TodoForm() {
     );
     e.target.reset();
   };
+
   return (
     <>
       <div className="max-w-7xl mx-auto mt-10">
@@ -66,8 +183,8 @@ function TodoForm() {
             ADD BOOKMARK
           </button>
         </form>
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
       </div>
-      -
     </>
   );
 }
